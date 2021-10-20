15ce060e7cdf5480ded18d276107008d13549076
Jonas Render <jonasrender@icloud.com>
2021-10-20 16:27:20 +0200

    inital commit

---
+++ src/main/java/htw/berlin/webtech/TogetherFit/TogetherFitApplication.java
@@ -0,0 +1,14 @@
+package htw.berlin.webtech.TogetherFit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherFitApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TogetherFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htw/berlin/webtech/TogetherFit/TogetherFitApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.berlin.webtech.TogetherFit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
