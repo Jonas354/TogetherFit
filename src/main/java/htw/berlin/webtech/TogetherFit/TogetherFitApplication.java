080923ca06c3fd12e247b779d75c90a282368cb7
Jonas Render <jonasrender@icloud.com>
2021-11-10 17:16:29 +0100

    github tests and fixes

--- src/main/java/htw/berlin/webtech/TogetherFit/TogetherFitApplication.java
+++ src/main/java/htw/berlin/webtech/TogetherFit/TogetherFitApplication.java
@@ -1,7 +1,9 @@
 package htw.berlin.webtech.TogetherFit;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class TogetherFitApplication {
